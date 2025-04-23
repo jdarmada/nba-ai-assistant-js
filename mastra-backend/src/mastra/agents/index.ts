d8eafd554411e8a663148bd96a2c4da231292233
jdarmada <jd.armada@elastic.co>
2025-04-22 18:12:24 -0700

    Added new tool and finetuned prompt for LLM instructions

--- mastra-backend/src/mastra/agents/index.ts
+++ mastra-backend/src/mastra/agents/index.ts
@@ -1,6 +1,7 @@
 import { openai } from '@ai-sdk/openai';
 import { Agent } from '@mastra/core/agent';
 import { playerComparisonTool } from '../tools';
+import { articleSemanticSearch } from '../tools';
 
 
 export const basketballAgent = new Agent({
@@ -8,20 +9,27 @@ export const basketballAgent = new Agent({
     instructions: `
       You are a NBA Basketball expert.
       
-      Your primary function is to compare two NBA players and recommend which one would be better for fantasy basketball.
-      When responding:
+      Your primary function is to compare two NBA players and recommend which one would be better for fantasy basketball. Require the user to input two players from this list.
+      The players available for comparison are LeBron James, Stephen Curry, Jason Tatum, Jaylen Brown, Nikola Jokic, Luka Doncic, Kyrie Irving, Anthony Davis, Kawhi Leonard and Russell Westbrook.
+      If the user asks about a player that is not on this list, respond with the list of available players for comparison.
+      If the user only inputs one player, ask the user to add another player from the list provided.
+      If the user inputs a player with the wrong spelling or capitalizations, infer from the list of available players provided.
+      When generating a response:
       - Extract the player names from the user's query
+      - Ensure that the formatting and spelling of the names are exactly as the list provided.
       - Use the playerComparisonTool provided and pass in the player names as strings
       - When using the playerComparisonTool, you will be returned an object, turn this object into a readable natural language response.
       - Continue to generate the response and send it back to the user after using the tool
       - Format the output like conversational dialogue with no markdown or special formatting
       - When mentioning historical and season averages, make sure to note that these stats are against that specific opponent
       - Mention the next game before the stats and also add whether it will be home or away
+      - Use the articleSemanticSearch tool and pass in the player names as strings.
+      - Include the returned articles under a section at the bottom of the response titled, "More information"
+      - Generate a text response on your recommendation on who is the best player to pickup in fantasy
       
-      If a user doesn't provide two player names, ask them to specify which players they want to compare.
     `,
     model: openai('gpt-4o'),
-    tools: {playerComparisonTool},
+    tools: {playerComparisonTool, articleSemanticSearch},
     
   });
 
