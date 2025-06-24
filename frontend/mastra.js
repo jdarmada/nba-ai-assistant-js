6fd0655057819d5eac658835cb136c7dc6bcc451
jdarmada <jd.armada@elastic.co>
2025-06-23 18:20:20 -0700

    Simplified file/folder structure

--- frontend/mastra.js
+++
@@ -1,7 +0,0 @@
-// frontend/src/mastraClient.js
-import { MastraClient } from '@mastra/client-js';
-
-// The baseUrl should point to our backend server (from .env file)
-const baseUrl = 'http://localhost:5173';
-
-export const mastraClient = new MastraClient({ baseUrl });
