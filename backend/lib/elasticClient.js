e1225899751f588bcf2ef6fc44ada06da272729c
jdarmada <jd.armada@elastic.co>
2025-06-24 03:21:54 -0700

    Added dotenv

--- backend/lib/elasticClient.js
+++ backend/lib/elasticClient.js
@@ -1,14 +1,25 @@
+import { config } from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import { Client } from '@elastic/elasticsearch';
 
+// Grab current directory and load .env from backend folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const envPath = join(__dirname, '../.env');
+
+// Load environment variables from the correct path
+config({ path: envPath });
+
 //Elastic client Initialization, make sure environment variables are being loaded in correctly
-const config= {
+const clientConfig = {
     node: `${process.env.ELASTIC_ENDPOINT}`,
     auth: {
         apiKey: `${process.env.ELASTIC_API_KEY}`,
     },
 };
 
-export const elasticClient = new Client(config);
+export const elasticClient = new Client(clientConfig);
 
 //Check if the client is connected
 async function checkConnection() { 
